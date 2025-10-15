bf39b3d962504f8133968443c5b020c17c142ec5
bantofweb4 <bantofweb4@gmail.com>
2025-10-15 12:59:04 +0100

    Fixed the header style import

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,51 +1,52 @@
-import './header.css'
-
+import styles from './Header.css'
 
 function Header() {
-    return (
-        <>
-            <div className="grid-two-cols">
-                <div>
-                <h1 className='connecting'>Connecting <br/> Belivers<br/> Strenthning <br /> Faith</h1>
-                </div>
+  return (
+    <>
+      <div className={styles['grid-two-cols']}>
+        <div>
+          <h1 className={styles.connecting}>
+            Connecting <br /> Believers <br /> Strengthening <br /> Faith
+          </h1>
+        </div>
 
-                <div className="header-img">
-                    <div className="headerimg2">
-                        <div class="headerimg1-sub">
-                            <div class="">
-                                <img src="img/dark guy.png" alt="" />
-                            </div>
-                            <div class="">
-                                <img src="img/darkman.png" alt="" />
-                            </div>    
-                        </div> 
-                    </div>   
-                    
-                    <div className="headerimg2">
-                        <div className="headerimg2-sub" >
-                            <div class="">
-                                <img src="img/darkwoman.png" alt=""/>
-                            </div>
-                            <div class="">
-                                <img src="img/braidman.png" alt=""/>
-                            </div>    
-                        </div> 
-                    </div>   
-
-                    <div className="headerimg2">
-                        <div className="headerimg3-sub">
-                            <div class="">
-                                <img src="img/glassman.png" alt=""/>
-                            </div>
-                            <div class="">
-                                <img src="img/green.png" alt=""/>
-                            </div>    
-                        </div> 
-                    </div>   
-                </div>
+        <div className={styles['header-img']}>
+          <div className={styles.headerimg2}>
+            <div className={styles['headerimg1-sub']}>
+              <div className={styles['']}>
+                <img src="img/dark guy.png" alt="" />
+              </div>
+              <div className={styles['']}>
+                <img src="img/darkman.png" alt="" />
+              </div>
             </div>
-        </>
-    );
+          </div>
+
+          <div className={styles.headerimg2}>
+            <div className={styles['headerimg2-sub']}>
+              <div className={styles['']}>
+                <img src="img/darkwoman.png" alt="" />
+              </div>
+              <div className={styles['']}>
+                <img src="img/braidman.png" alt="" />
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.headerimg2}>
+            <div className={styles['headerimg3-sub']}>
+              <div className={styles['']}>
+                <img src="img/glassman.png" alt="" />
+              </div>
+              <div className={styles['']}>
+                <img src="img/green.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Header
