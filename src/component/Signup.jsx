99dd8fd52bc22b1b7c62a6cd34e9fb2ff58dbc15
bantofweb4 <bantofweb4@gmail.com>
2025-10-16 11:34:55 +0100

    I made another edit in the sign up

--- src/component/Signup.jsx
+++ src/component/Signup.jsx
@@ -5,34 +5,34 @@ function Signup() {
     return (
         <>
             <div className="signup-container">
-            <div className="signup-left">
-                <form className="signup-form">
-                    <img src="img/logo.png" alt="Faith" className="logo" />
-                    <h1>Create an account</h1>
+                <div className="signup-left">
+                    <form className="signup-form">
+                        <img src="img/logo.png" alt="Faith" className="logo" />
+                        <h1>Create an account</h1>
 
-                    <label>Full Name</label><br />
-                    <input type="text" className="input-field" required /><br />
+                        <label>Full Name</label><br />
+                        <input type="text" className="input-field" required /><br />
 
-                    <label>Email</label><br />
-                    <input type="email" className="input-field" required /><br />
+                        <label>Email</label><br />
+                        <input type="email" className="input-field" required /><br />
 
-                    <label>Password</label><br />
-                    <input type="password" className="input-field" required /><br />
+                        <label>Password</label><br />
+                        <input type="password" className="input-field" required /><br />
 
-                    <button type="submit" className="submit-btn">Submit</button><br />
+                        <button type="submit" className="submit-btn">Submit</button><br />
 
-                    <img src="img/google.png" alt="Sign in with Google" className="google" />
+                        <img src="img/google.png" alt="Sign in with Google" className="google" />
 
-                    <p className="login-note">
-                    Have an account? <a href="log in.html">Log in</a>
-                    </p>
-                </form>
+                        <p className="login-note">
+                        Have an account? <a href="log in.html">Log in</a>
+                        </p>
+                    </form>
                 </div>
 
                 <div className="signup-right">
-                <img src="img/Mask group-1.png" alt="" />
-                <img src="img/Mask group-2.png" alt="" />
-                <img src="img/Mask group.png" alt="" />
+                    <img src="img/Mask group-1.png" alt="" />
+                    <img src="img/Mask group-2.png" alt="" />
+                    <img src="img/Mask group.png" alt="" />
                 </div>
             </div>
         </>
