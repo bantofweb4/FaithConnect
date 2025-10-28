586f7a05393bf8794ede49c17f2d1fa0fa45ac05
bantofweb4 <bantofweb4@gmail.com>
2025-10-28 20:54:52 +0100

    changees

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -5,12 +5,10 @@ function Nav() {
   return (
     <>
         <nav>
-            <div className="Logo"><img src="img/logo.png" alt="" /></div>
+            <Link to="/"><div className="Logo"><img src="img/logo.png" alt="" /></div></Link>
             <ul className="flex gap-[40px] ">
-                <li className="" ><Link to="/">Home</Link></li>
                 <li className="" ><Link to="/podcast">Podcasts</Link></li>
                 <li className="" ><Link to="/bookspage">Books</Link></li>
-                <li className="" ><Link to="">Blogs</Link></li>
                 <li className="" ><Link to="/Aboutpage">About</Link></li>
                 <div className="bg-[#FFB825] rounded-tl-[15px] rounded-bl-[15px] rounded-tr-[15px] rounded-br-none h-[30px] w-[60px] mx-[20px] px-[20px]">
                     <li className="underline" ><Link to="/mainsignup">Sign Up</Link></li>
