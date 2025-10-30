b9967584ae8d870c7083fcd4b04b2a5eb08296e9
bantofweb4 <bantofweb4@gmail.com>
2025-10-30 19:42:46 +0100

    chnagesss

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -7,7 +7,7 @@ function Nav() {
         <nav>
             <Link to="/"><div className="Logo"><img src="img/logo.png" alt="" /></div></Link>
             <ul className="navul">
-                <li className="" ><Link to="/podcast">Podcasts</Link></li>
+                <li className="navli" ><Link to="/podcast">Podcasts</Link></li>
                 <li className="" ><Link to="/bookspage">Books</Link></li>
                 <li className="" ><Link to="/Aboutpage">About</Link></li>
                 <div className="bg-[#FFB825] rounded-tl-[15px] rounded-bl-[15px] rounded-tr-[15px] rounded-br-none h-[30px] w-[60px] mx-[20px] px-[20px]">
