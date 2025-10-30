eae6677e34291469f6542619aeb60999399ee4c9
bantofweb4 <bantofweb4@gmail.com>
2025-10-30 20:08:13 +0100

    change

--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -10,7 +10,7 @@ function Nav() {
                 <li className="navli" ><Link to="/podcast">Podcasts</Link></li>
                 <li className="" ><Link to="/bookspage">Books</Link></li>
                 <li className="" ><Link to="/Aboutpage">About</Link></li>
-                <div className="bg-[#FFB825] rounded-tl-[15px] rounded-bl-[15px] rounded-tr-[15px] rounded-br-none h-[30px] w-[60px] mx-[20px] px-[20px]">
+                <div className="signup-box">
                     <li className="underline" ><Link to="/mainsignup">Sign Up</Link></li>
                 </div>
             </ul>
